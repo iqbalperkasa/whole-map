14ec952b4a3ec19a7d213a1ab1e1720bcfe78133
Iqbal <iqbal@luno.com>
2023-12-11 01:20:51 +0700

    slide 2 responsive

--- components/Slide2.vue
+++ components/Slide2.vue
@@ -14,13 +14,13 @@ function scrollRight() {
 </script>
 
 <template>
-  <div class="flex items-center justify-end bg-[url('/bg-globe/img-1920x1200.jpg')] bg-no-repeat bg-contain bg-black bg-center h-screen text-white animate__animated animate__fadeIn">
-    <div class="max-w-[800px] mx-24">
+  <div class="flex items-center justify-end bg-[url('/bg-globe/img-800x368.jpg')] lg:bg-[url('/bg-globe/img-1920x1200.jpg')] bg-no-repeat bg-contain bg-black bg-center h-screen text-white animate__animated animate__fadeIn">
+    <div class="w-screen lg:w-[800px] pr-24 lg:pr-0 lg:mx-24 border border-red-500">
       <div class="text-4xl text-right mb-4 mr-8 font-bold">{{ content?.title }}</div>
-      <div class="flex items-center">
+      <div class="flex items-center border border-red-500">
         <div class="border-[20px] border-transparent border-r-white cursor-pointer"></div>
         <div
-          class="w-[600px] mx-4 overflow-scroll flex gap-4 text-black rounded-lg"
+          class="border border-red-500 mx-4 overflow-scroll flex gap-4 text-black rounded-lg"
           ref="scroller"
         >
           <div
